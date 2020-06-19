e781e987a812b04854449b7b1caa93cfbbe0117a
Tina <919457987@qq.com>
2020-06-19 10:45:20 +0000

    123

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+		public int SuppressJitTest()
+		{
+			int result = 0;
+
+			for (int i = 0; i < 10; i++)
+			{
+				int nouse = 10;
+
+				result += i;
+			}
+
+			return result;
+		}
+	}
+}
---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,15 @@
+﻿using ClassLibrary1;
+using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 classs = new Class1();
+            classs.SuppressJitTest();
+        Console.WriteLine("Hello World!");
+        }
+    }
+}
